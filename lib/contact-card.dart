0ced7deeb609a4e161ccb41efd9261e7ba0ab9c9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-29 18:40:13 +0300

    Revert "Update contact-card.dart"
    This reverts commit f5291d03f152754d2cd028abb412e0c5919eb746.

--- lib/contact-card.dart
+++ lib/contact-card.dart
@@ -2,18 +2,8 @@ import 'package:flutter/material.dart';
 import 'models/contact.dart';
 
 class ContactCard extends StatelessWidget {
-  final int? index;
   final Contact? contact;
-  final Function? updateCallback;
-  final Function? deleteCallback;
-
-  const ContactCard(
-      {Key? key,
-      this.index,
-      this.contact,
-      this.updateCallback,
-      this.deleteCallback})
-      : super(key: key);
+  const ContactCard({Key? key, this.contact}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -53,27 +43,16 @@ class ContactCard extends StatelessWidget {
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              GestureDetector(
-                onTap: deleteCallback!(),
-                child: Icon(
-                  Icons.delete,
-                  color: Colors.grey[600],
-                ),
+              Icon(
+                Icons.create,
+                color: Colors.grey[600],
               ),
-              SizedBox(width: 15.0),
-              GestureDetector(
-                onTap: () => updateCallback!(index, contact),
-                child: Icon(
-                  Icons.create,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(width: 15.0),
+              const SizedBox(width: 15.0),
               Icon(
                 Icons.message,
                 color: Colors.grey[600],
               ),
-              SizedBox(width: 15.0),
+              const SizedBox(width: 15.0),
               Icon(
                 Icons.call,
                 color: Colors.grey[600],
