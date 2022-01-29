e4f1986eca664169fb5c050b812430bbac0f2ec0
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-29 14:54:25 +0300

    Update contact.dart

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,13 +1,13 @@
 class Contact {
-  final String firstName;
-  final String lastName;
-  final String imageUrl;
-  final String phone;
+  final String? firstName;
+  final String? lastName;
+  final String? imageUrl;
+  final String? phone;
 
   Contact({
-    required this.firstName,
-    required this.lastName,
-    required this.imageUrl,
-    required this.phone,
+     this.firstName,
+     this.lastName,
+     this.imageUrl,
+     this.phone,
   });
 }
