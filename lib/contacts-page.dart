17f372650483e8c5b50784e11e3f34a82768b72c
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-29 14:54:23 +0300

    Update contacts-page.dart

--- lib/contacts-page.dart
+++ lib/contacts-page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dummydata.dart';
+import 'models/contact.dart';
+
 class ContactsPage extends StatefulWidget {
   const ContactsPage({Key? key}) : super(key: key);
 
@@ -8,6 +11,14 @@ class ContactsPage extends StatefulWidget {
 }
 
 class _ContactsPageState extends State<ContactsPage> {
+  Future<List<Contact>>? _contacts;
+
+  Future<List<Contact>> _getContacts() async {
+    List<Contact> contacts = dummyData;
+    await Future.delayed(Duration(seconds: 10));
+    return Future.value(contacts);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
